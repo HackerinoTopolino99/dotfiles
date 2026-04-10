de875dbdbf089b8b2a835d3bc68ef8929d801d63
HackerinoTopolino99 <giuliano.vallone@ikmail.com>
2026-04-11 00:23:54 +0200

    refactoring

--- nvim/.config/nvim/lua/config/mapping.lua
+++ nvim/.config/nvim/lua/config/mapping.lua
@@ -18,6 +18,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- Usefull keybindings
 vim.keymap.set({ "n" }, "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set({ "n" }, "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set({ "n" }, "t", "<C-w><CR><C-w>T")
 
 -- NeoTree Mappings
 vim.keymap.set({ "n", "v", "i" }, "<C-b>", "<cmd>Neotree toggle action=show<CR>")
@@ -27,9 +28,27 @@ vim.keymap.set({ "n" }, "<leader>e", "<cmd>Neotree reveal<CR>")
 vim.keymap.set({ "n", "v" }, "<leader>t", "<cmd>ToggleTerm<CR>")
 
 -- LSP Mappings
-vim.keymap.set({ "n" }, "K", vim.lsp.buf.hover, {})
-vim.keymap.set({ "n" }, "<leader>gd", vim.lsp.buf.definition, {})
-vim.keymap.set({ "n" }, "<leader>gr", vim.lsp.buf.references, {})
+vim.keymap.set({ "n" }, "<leader>k", vim.lsp.buf.hover, {})
+
+vim.keymap.set({ "n" }, "<leader>gd", function()
+  vim.lsp.buf.definition({
+		on_list = function(options)
+			if #options.items > 0 then
+				local item = options.items[1]
+        local current_file = vim.api.nvim_buf_get_name(0)
+
+        if item.filename ~= current_file then
+			    vim.cmd("tab split")
+        end
+
+				vim.cmd("edit " .. item.filename)
+				vim.api.nvim_win_set_cursor(0, { item.lnum, math.max(0, item.col - 1) })
+			end
+		end,
+	})
+end, {buffer = true, silent = true})
+
+vim.keymap.set({ "n" }, "<leader>gr", vim.lsp.buf.references)
 vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set({ "n" }, "<leader>rn", vim.lsp.buf.rename, {})
 vim.keymap.set({ "n" }, "<leader>f", vim.lsp.buf.format)
