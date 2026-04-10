64cf797e479060dbcddb78d255408a5dbc575940
HackerinoTopolino99 <giuliano.vallone@ikmail.com>
2026-04-10 23:03:03 +0200

    Using nvim-treesitter to install parsers

---
+++ nvim/.config/nvim/plugin/nvim-treesitter.lua
@@ -0,0 +1,17 @@
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+
+require("nvim-treesitter").setup()
+require("nvim-treesitter").install({
+  "asm",
+  "bash",
+  "dockerfile",
+  "gitignore",
+	"go",
+  "hcl",
+  "java",
+	"jinja",
+	"jinja_inline",
+	"python",
+  "ssh_config",
+	"yaml",
+})
