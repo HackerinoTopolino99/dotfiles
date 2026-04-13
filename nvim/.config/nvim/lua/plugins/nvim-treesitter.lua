e2ffb7ab29c065118b303d57b852b7fc3ca8399b
HackerinoTopolino99 <giuliano.vallone@ikmail.com>
2026-04-13 20:10:24 +0200

    Added missing parsers. Created autocommand to upgrade automatically parsers when nvim-treesitter is upgraded

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,4 +1,6 @@
-vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
+vim.pack.add({
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+})
 
 require("nvim-treesitter").setup()
 require("nvim-treesitter").install({
@@ -9,15 +11,45 @@ require("nvim-treesitter").install({
 	"gitignore",
 	"go",
 	"hcl",
+	"hyprlang",
+	"ini",
 	"java",
+	"javascript",
 	"jinja",
 	"jinja_inline",
+	"json",
 	"latex",
+	"lua",
 	"make",
+	"markdown",
+	"markdown_inline",
 	"nginx",
+	"objdump",
 	"python",
+	"requirements",
 	"ssh_config",
 	"terraform",
+	"typescript",
 	"yaml",
-    "zsh"
+	"zsh",
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+	pattern = "*",
+
+	callback = function(ev)
+		local data = ev.data
+		if not data or not data.spec or data.kind ~= "update" then
+			return
+		end
+
+		local spec_str = type(data.spec) == "string" and data.spec or (data.spec.src or data.spec.name or "")
+
+		if spec_str:match("nvim-treesitter") then
+			vim.schedule(function()
+				vim.cmd("packadd nvim-treesitter")
+				vim.cmd("TSUpdate")
+			end)
+		end
+	end,
 })
