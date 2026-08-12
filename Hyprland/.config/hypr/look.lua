ffe8c4d781c459849345dc3830eb8a6c647bf485
HackerinoTopolino99 <giuliano.vallone@ikmail.com>
2026-08-12 17:44:55 +0200

    Refactoring

--- Hyprland/.config/hypr/look.lua
+++ Hyprland/.config/hypr/look.lua
@@ -118,14 +118,3 @@ hl.config({
         fullscreen_on_one_column = true,
     },
 })
-
-----------------
-----  MISC  ----
-----------------
-
-hl.config({
-    misc = {
-        force_default_wallpaper = 2,    -- Set to 0 or 1 to disable the anime mascot wallpapers
-        disable_hyprland_logo   = false, -- If true disables the random hyprland logo / anime girl background. :(
-    },
-})
